31b17705a388902a88c86fbabcb0127564592d98
Chris Mann <chrispmann@TP-Dorayaki>
2023-06-06 13:59:20 -0700

    edited scraper generator

--- scraper-generator.py
+++ scraper-generator.py
@@ -21,9 +21,9 @@ agent = initialize_agent(tools, chat, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCR
 results = agent.run(
             """You are a job searching agent. Follow the following steps:
             1. Scan the page https://www.thesoftwarereport.com/the-top-100-software-companies-of-2022/ 
-            2. Google the careers page of the first entry
-            3. Create a Python function to scrape the careers page for the following information: company name, job title, job description URL
-            4. Run the function using PytonREPL and create a JSON object from its output
-            5. output the function and JSON object
+            2. Google the careers page of a random company on that list
+            3. Create a Python function to scrape the careers page for the following information about each job on it: company name, job title, job description URL
+            4. Run the function using PythonREPL to confirm it works
+            5. output the function
             """
 )
