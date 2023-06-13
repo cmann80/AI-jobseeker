8696b185b123a9da1e9bd05b5ae7593971fa1bf5
Chris Mann <chrispmann@gmail.com>
2023-06-12 18:01:01 -0700

    last commit of dev branch

--- direct-scan.py
+++ direct-scan.py
@@ -21,7 +21,7 @@ agent = initialize_agent(tools, chat, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCR
 results = agent.run(
             """You are a job searching agent. Follow the following steps:
             1. Scan the page https://www.thesoftwarereport.com/the-top-100-software-companies-of-2022/ 
-            2. Google careers for that company that contain the string "generative AI" and read the job listing
+            2. Google careers for a random company on that list that contain the string "generative AI"
             3. If one is found, return a JSON object with the following keys: Company name, position name, URL link to the job posting
             4. If none are found, return "none found"
             """
